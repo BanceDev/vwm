2e5dcf7e3227f4b0a528a9a44fcf50f8e36daf45
BanceDev <lanceaborden@protonmail.com>
2025-04-05 01:41:46 -0400

    update install script

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 sudo cp ./assets/vwm /usr/bin
 sudo cp ./assets/vwm-session /usr/bin
 sudo cp ./assets/vwm-session.desktop /usr/share/xsessions
-sudo cp -r ./src /usr/bin/vwm-src
+sudo mkdir -p /usr/bin/vwm-src
+sudo cp -r ./src/* /usr/bin/vwm-src
